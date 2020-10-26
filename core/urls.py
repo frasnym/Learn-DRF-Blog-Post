022799fc2e8b449f5ba762a83516a07e3b2f91fb
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-26 14:41:29 +0800

    Function Based API Views

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import article_list
+from .views import article_list, article_detail
 
 
 urlpatterns = [
-    path('articles/', article_list),
+    path('function-based-articles/', article_list),
+    path('function-based-article-detail/<int:pk>/', article_detail),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
 from rest_framework.parsers import JSONParser
@@ -20,21 +20,45 @@ def article_list(request):
         serializer = ArticleSerializer(articles, many=True)
         
         return JsonResponse(serializer.data, safe=False)
-
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ArticleSerializer(data=data)
 
         if serializer.is_valid():
             serializer.save()
-
             return JsonResponse(serializer.data, status=201)
-        
+
         return JsonResponse(serializer.errors, status=400)
+    else:
+        return HttpResponse(status=400)
+
+
+@csrf_exempt
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data, safe=False)
+    
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return HttpResponse(status=204)
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
-
-
