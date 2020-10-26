cc80fb4fa85277ca144e3e2e0dd75570e54a83f5
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-26 15:11:23 +0800

    api_view() decorators

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import article_list, article_detail
+from .views import (
+    article_list_function_based,
+    article_detail_function_based,
+    article_list_api_view_decorators,
+    article_detail_api_view_decorators,
+)
 
 
 urlpatterns = [
-    path('function-based-articles/', article_list),
-    path('function-based-article-detail/<int:pk>/', article_detail),
+    path('function-based-articles/', article_list_function_based),
+    path('function-based-article-detail/<int:pk>/',
+         article_detail_function_based),
+
+    path('api-view-decorators-articles/', article_list_api_view_decorators),
+    path('api-view-decorators-article-detail/<int:pk>/', article_detail_api_view_decorators),
 ]
--- core/views.py
+++ core/views.py
@@ -2,11 +2,53 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
+from rest_framework import viewsets, generics, mixins, status
 from rest_framework.parsers import JSONParser
-from rest_framework import viewsets, generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Article
 from .serializers import ArticleSerializer
+
+@api_view(['GET', 'POST'])
+def article_list_api_view_decorators(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail_api_view_decorators(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 """
 Note that because we want to be able to POST to this view from clients that won't have a CSRF token we need to 
@@ -14,7 +56,7 @@ mark the view as csrf_exempt. This isn't something that you'd normally want to d
 actually use more sensible behavior than this, but it'll do for our purposes right now.
 """
 @csrf_exempt
-def article_list(request):
+def article_list_function_based(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
@@ -34,7 +76,7 @@ def article_list(request):
 
 
 @csrf_exempt
-def article_detail(request, pk):
+def article_detail_function_based(request, pk):
     try:
         article = Article.objects.get(pk=pk)
     except Article.DoesNotExist:
