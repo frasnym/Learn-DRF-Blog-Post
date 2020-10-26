71144c4333533f048c18bee370cf0fa6f35755f1
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-26 15:46:55 +0800

    Class Based API Views

--- core/urls.py
+++ core/urls.py
@@ -5,14 +5,22 @@ from .views import (
     article_detail_function_based,
     article_list_api_view_decorators,
     article_detail_api_view_decorators,
+    ArticleAPIView,
+    ArticleDetailAPIView,
 )
 
 
 urlpatterns = [
+    # Function Based API Views
     path('function-based-articles/', article_list_function_based),
     path('function-based-article-detail/<int:pk>/',
          article_detail_function_based),
 
+    # api_view() decorators
     path('api-view-decorators-articles/', article_list_api_view_decorators),
     path('api-view-decorators-article-detail/<int:pk>/', article_detail_api_view_decorators),
+
+    # Class Based API Views
+    path('class-based-articles/', ArticleAPIView.as_view()),
+    path('class-based-article-detail/<int:id>/', ArticleDetailAPIView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -6,9 +6,53 @@ from rest_framework import viewsets, generics, mixins, status
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Article
 from .serializers import ArticleSerializer
+
+
+class ArticleAPIView(APIView):
+
+    def get(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailAPIView(APIView):
+
+    def get_object(self, id):
+        try:
+            return Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        article = self.get_object(id)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        article = self.get_object(id)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 @api_view(['GET', 'POST'])
 def article_list_api_view_decorators(request):
@@ -27,6 +71,7 @@ def article_list_api_view_decorators(request):
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
 @api_view(['GET', 'PUT', 'DELETE'])
 def article_detail_api_view_decorators(request, pk):
     try:
@@ -37,7 +82,7 @@ def article_detail_api_view_decorators(request, pk):
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
-    
+
     elif request.method == 'PUT':
         serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
@@ -60,7 +105,7 @@ def article_list_function_based(request):
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        
+
         return JsonResponse(serializer.data, safe=False)
     elif request.method == 'POST':
         data = JSONParser().parse(request)
@@ -85,7 +130,7 @@ def article_detail_function_based(request, pk):
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
         return JsonResponse(serializer.data, safe=False)
-    
+
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = ArticleSerializer(article, data=data)
